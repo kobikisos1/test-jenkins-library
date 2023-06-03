e811aaeff144c1cfb47bf31a3ed95b77939bb7fe
kobikisos <kobi@sensi.ai>
2023-06-03 08:38:29 +0300

    refactor

--- vars/build.groovy
+++ vars/build.groovy
@@ -4,5 +4,5 @@ def call(Map param = [:]) {
     ]
     config << param
 
-
+    deployFlow(config)
 }
---
+++ vars/deployFlow.groovy
@@ -0,0 +1,8 @@
+import groovy.json.JsonSlurper
+
+
+def call(Map config) {
+        stage('Checkout') {
+            echo "Checkout"
+        }
+}
